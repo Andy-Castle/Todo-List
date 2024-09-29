3e087b1681d9f4bbbffffa7d65adbe043e7857c1
Andy-Castle <masterchief_and@live.com>
2024-09-29 01:17:16 -0500

    ok ya devuele una esto bien creo

--- src/project&todos.js
+++ src/project&todos.js
@@ -1,50 +1,58 @@
 let arrayProjects = [];
 
 class Project {
+  static id = 0;
   constructor(name) {
-    this.id = arrayProjects.length + 1;
+    this.id = ++Project.id;
     this.name = name;
     this.todos = [];
-    return arrayProjects.push({ id: this.id, name: this.name, todos: this.todos });
+
+    return {
+      id: this.id,
+      name: this.name,
+      todos: this.todos,
+      addTodo(todo) {
+        this.todos.push(todo);
+      },
+    };
   }
 }
 
-class TodosProject extends Project {
-  constructor(title, description, dueDate, priority, status, todos) {
-    super(todos);
-    this.id = this.todos.length + 1;
+class TodosProject {
+  static id = 0;
+  constructor(title, description, dueDate, priority, status) {
+    this.id = ++TodosProject.id;
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.status = status;
 
-    return todos.push({
+    return {
       id: this.id,
+      title: this.title,
       description: this.description,
       dueDate: this.dueDate,
       priority: this.priority,
       status: this.status,
-    });
+    };
   }
 }
 
-new Project("Demo");
-new Project("Default");
+let project = new Project("Default");
 
-new TodosProject(
-  "Importante",
-  "Primera descripción",
+let todo = new TodosProject(
+  "Add a list",
+  "First description",
   "28/09/2024",
   "Low",
   "To Do"
 );
 
+arrayProjects.push(project);
+
+project.addTodo(todo);
+
 console.log(arrayProjects);
 
-function buscarObjeto(name) {
-  return arrayProjects.find((n) => n.name == name);
-}
-
-console.log(buscarObjeto("Demo").todos);
 export { arrayProjects, Project, TodosProject };
