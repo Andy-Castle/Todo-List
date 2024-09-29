5be22e6a6e4ef4b1d4b1fdb34bf1c51a6962dca5
Andy-Castle <masterchief_and@live.com>
2024-09-28 23:56:26 -0500

    ya puedo crear todos y asignarlos al proyecto designado

--- src/project.js
+++ src/project.js
@@ -9,16 +9,46 @@ let arrayProjects = [];
 //   }
 // }
 
-function Project(name) {
-  this.id = arrayProjects.length + 1;
-  this.name = name;
-  this.todos = [];
-  return arrayProjects.push({ id: this.id, name: this.name, todos: this.todos });
+class Project {
+  constructor(name) {
+    this.id = arrayProjects.length + 1;
+    this.name = name;
+    this.todos = [];
+    return arrayProjects.push({ id: this.id, name: this.name, todos: this.todos });
+  }
 }
 
 let project = new Project("Demo");
-let projects = new Project("Demo");
-let projectss = new Project("Demo");
+
+// }
+
+class TodosProject {
+  constructor(title, description, dueDate, priority, status, todos) {
+    this.id = todos.length + 1;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.status = status;
+    return todos.push({
+      id: this.id,
+      description: this.description,
+      dueDate: this.dueDate,
+      priority: this.priority,
+      status: this.status,
+    });
+  }
+}
+
+let newTodo = new TodosProject(
+  "Importante",
+  "Primera descripción",
+  "28/09/2024",
+  "Bajo",
+  "Doing",
+  project.todos
+);
+
+console.log(newTodo);
 
 console.log(arrayProjects);
-// }
