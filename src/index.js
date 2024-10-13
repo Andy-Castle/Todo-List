d79793bd1f28b3001badd99309cbebba994c14e6
Andy-Castle <masterchief_and@live.com>
2024-10-13 00:29:57 -0500

    por ahora llevo esto

--- src/index.js
+++ src/index.js
@@ -10,33 +10,33 @@ import {
 
 const TodoList = document.getElementById("todo-list");
 
-function CreateTemplate() {
-  TodoList.innerHTML = `<div class="ToDo">
-            <h1>To Do</h1>
-            <div
-              id="To_Do"></div>
-          </div>
-          <div class="Doing">
-            <h1>Doing</h1>
-            <div id="Doing"></div>
-          </div>
-          <div class="Done">
-            <h1>Done</h1>
-            <div id="Done"></div>
-          </div>`;
-}
-CreateTemplate();
+// function CreateTemplate() {
+//   TodoList.innerHTML = `<div class="ToDo">
+//             <h1>To Do</h1>
+//             <div
+//               id="To_Do"></div>
+//           </div>
+//           <div class="Doing">
+//             <h1>Doing</h1>
+//             <div id="Doing"></div>
+//           </div>
+//           <div class="Done">
+//             <h1>Done</h1>
+//             <div id="Done"></div>
+//           </div>`;
+// }
+// CreateTemplate();
 
-function addCard() {
-  const ListToDo = document.getElementById("To_Do");
+// function addCard() {
+//   const ListToDo = document.getElementById("To_Do");
 
-  ListToDo.innerHTML = `<div
-  id="1"
-  class="card"
- >
-  Thing
-</div>
-`;
-}
+//   ListToDo.innerHTML = `<div
+//   id="1"
+//   class="card"
+//  >
+//   Thing
+// </div>
+// `;
+// }
 
 addCard();
